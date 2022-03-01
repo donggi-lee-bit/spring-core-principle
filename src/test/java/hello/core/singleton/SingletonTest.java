15697eb7b77c8517e771cfa037764d3a20f78d3a
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-01 22:22:11 +0900

    싱글톤 패턴을 적용한 객체 테스트
    - 생성자를 private으로 하여 외부에서 생성할 수 없게 만들어놓는다.
    - 객체를 내부에서 한 개 생성하여 메서드를 이용하여 해당 객체만 호출할 수 있게 해둔다.
    - 이렇게 싱글톤 패턴을 만들고 싱글톤 패턴을 이용하면 여러 개의 객체를 만들지 않고도 여러 사용자가 동시에 객체를 사용할 수 있게된다.
    - 하지만 생각보다 싱글톤 패턴을 이용하면 단점이 많이 있었다.
    - 이걸 해결해주는 게 스프링의 싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,4 +1,4 @@
-package hello.core.singletone;
+package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletoneTest {
+public class SingletonTest {
     
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -24,5 +24,17 @@ public class SingletoneTest {
 
         // memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+    
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
