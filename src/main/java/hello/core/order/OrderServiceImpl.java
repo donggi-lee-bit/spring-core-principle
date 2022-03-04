a3ec463f4f77ea18dee1cd6faada1809e99cf3f2
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-04 12:05:19 +0900

    애노테이션 만들어보기
    - @Qualifier("mainDiscountPolicy") 문자를 적으면 오타가 나도 컴파일시 체크가 안된다. 그래서 애노테이션을 따로 만드는 게 낫다
    - @Qualifier 내부로 들어가서 애노테이션이 달려있는걸 복붙해서 가져와 애노테이션을 만들어준다
    - 이렇게 여러 애노테이션을 모아 사용하는 기능은 스프링이 지원해준다. 자바에서는 안됨. 다른 애노테이션도 조합해서 만들 수 있다. 하지만 무분별하게 재정의하는 것은 유지보수에 혼란만 더 줄 수 있다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -12,9 +13,9 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = rateDiscountPolicy;
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
